cab996c0e68057b775384e103336736de476c1bd
Tanish Makadia <tanishmakadia@gmail.com>
2024-06-05 02:54:48 -0400

    sketchybar: Shorten media label length

--- home/.config/sketchybar/constants.sh
+++ home/.config/sketchybar/constants.sh
@@ -4,8 +4,8 @@
 export VOLUME_BAR_WIDTH=63
 
 # Parameters controlling the length of the media label
-export MEDIA_MAX_CHARS=20                                   # Maximum number of characters in media label
-export VOLUME_SHRINK=8                                      # Amount to subtract if volume is extended
+export MEDIA_MAX_CHARS=16                                   # Maximum number of characters in media label
+export VOLUME_SHRINK=6                                      # Amount to subtract if volume is extended
 export WIFI_SHRINK=8                                        # Amount to subtract if wifi is extended
 export MAX_CHARS_FILE="/tmp/sketchybar_media_max_chars.txt" # File to store the current max chars
 
